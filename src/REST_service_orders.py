cf392c397238a429babb95811bfff9ab165f41e2
Jannis Bittmayer <ge49foh@mytum.de>
2024-03-17 17:00:00 +0100

    Refactor order matching and rule handling logic

--- src/REST_service_orders.py
+++ src/REST_service_orders.py
@@ -24,36 +24,57 @@ create_tables()
 @route('/order')
 def index():
     with sqlite3.connect('database.db') as conn:
-        c = conn.cursor()
+        db_cursor = conn.cursor()
         message = request.query.message
         user_id = request.query.user_id
         timestamp = request.query.timestamp
         new_order = Order(message, user_id, timestamp)
-        # Check, if matching rule can be found
-        words = message.split()
-        # For every word, check if a matching rule can be found
-        for word in words:
-            # Ignore short words like "a", "the", "is", etc.
-            if len(word) < 4:
-                continue
-            # Check if a matching rule can be found
-            c.execute('SELECT * FROM rules WHERE strings_to_match LIKE :strings',
-                      {'strings': '%' + word + '%'})
-            matching_rule = c.fetchone()
-            # If rule is found, send a request to the matching rule's callback and delete the rule from the database
-            if matching_rule:
-                # Send a request to the matching rule's callback
-                callback_url = matching_rule[1]
-                requests.put(callback_url, json.dumps(
-                    {'user_id': user_id, 'cocktail': word, 'timestamp': timestamp}))
-                # Delete the rule from the database
-                c.execute('DELETE FROM rules WHERE strings_to_match = :strings AND callback = :callback', {
-                          'strings': matching_rule[0], 'callback': matching_rule[1]})
-                return 'Rule matched: Found ' + word + ' in ' + matching_rule[0]
-        # If no rule is found, save the order in the database
-        c.execute('INSERT INTO orders VALUES (:message, :user_id, :timestamp)', {
-                  'message': message, 'user_id': user_id, 'timestamp': timestamp})
+        # Check, whether order is already matched by a rule
+        matched_word, matching_rule = find_matching_rule(db_cursor, message)
+        # If rule is found, callback CPEE, delete rule and return info to user
+        if matching_rule:
+            send_request_and_delete_rule(
+                db_cursor, matching_rule, user_id, timestamp)
+            return 'Rule matched: Found \"' + matched_word + '\" in rule ' + matching_rule[0]
+        else:
+            save_order(db_cursor, message, user_id, timestamp)
     return 'No matching rule found, order saved in order queue'
+
+# Find matching rule in database
+
+
+def find_matching_rule(db_cursor, message):
+    words = message.split()
+    for word in words:
+        # Ignore words with less than 4 characters like 'a', 'the', 'and'
+        if len(word) < 4:
+            continue
+        # Check if a matching rule can be found
+        db_cursor.execute('SELECT * FROM rules WHERE strings_to_match LIKE :strings',
+                          {'strings': '%' + word + '%'})
+        matching_rule = db_cursor.fetchone()
+        if matching_rule:
+            return word, matching_rule
+    return None, None
+
+# Tell CPEE about matched rule and delete rule from database
+
+
+def send_request_and_delete_rule(db_cursor, matching_rule, user_id, timestamp):
+    callback_url = matching_rule[1]
+    # Tell CPEE about matched rule
+    requests.put(callback_url, json.dumps(
+        {'user_id': user_id, 'cocktail': matching_rule[0], 'timestamp': timestamp}))
+    # Delete matched rule from database
+    db_cursor.execute('DELETE FROM rules WHERE strings_to_match = :strings AND callback = :callback', {
+        'strings': matching_rule[0], 'callback': matching_rule[1]})
+
+# Save order to database
+
+
+def save_order(db_cursor, message, user_id, timestamp):
+    db_cursor.execute('INSERT INTO orders VALUES (:message, :user_id, :timestamp)', {
+        'message': message, 'user_id': user_id, 'timestamp': timestamp})
 
 
 run(host='::1', port=49124)
