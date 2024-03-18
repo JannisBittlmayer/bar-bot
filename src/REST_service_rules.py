36705b4f4c5c0b9b621ce6bd05a4f2fb71adb6b4
jannis bittlmayer <jannis.bittlmayer@tum.de>
2024-03-18 12:43:01 +0100

    Refactor REST_service_rules.py to improve readability

--- src/REST_service_rules.py
+++ src/REST_service_rules.py
@@ -1,7 +1,6 @@
 import json
 from bottle import route, run, request, response
 import sqlite3
-import requests
 from rule import Rule
 
 conn = sqlite3.connect('database.db')
@@ -19,36 +18,51 @@ with conn:
 
 @route('/add_rule')
 def index():
-    callback_url = request.headers.get('Cpee-Callback')
-    strings_to_match = request.query.strings_to_match
-    # Create new Rule object
-    new_rule = Rule(strings_to_match, callback_url)
-    # Check, whether rule is already matched by a order
+    with sqlite3.connect('database.db') as conn:
+        db_cursor = conn.cursor()
+        callback_url = request.headers.get('Cpee-Callback')
+        strings_to_match = request.query.strings_to_match
+        new_rule = Rule(strings_to_match, callback_url)
+        # Check, whether rule is already matched by an order
+        matching_order = find_matching_order(db_cursor, strings_to_match)
+        # If order is found, delete order and return info to user
+        if matching_order:
+            delete_order(db_cursor, matching_order)
+            return json.dumps({'user_id': matching_order[1], 'cocktail': strings_to_match, 'timestamp': matching_order[2]})
+        # If no order is found, save the rule and tell CPEE to wait for it to be fulfilled
+        save_rule(db_cursor, strings_to_match, callback_url)
+        response.set_header('Cpee-Callback', 'true')
+    return 'No matching order found, rule saved in rule queue'
+
+
+# Find matching order in database
+
+def find_matching_order(db_cursor, strings_to_match):
     cocktails = strings_to_match.split(',')
     for cocktail in cocktails:
-        # Check if a matching order can be found
-        with conn:
-            c.execute('SELECT * FROM orders WHERE message LIKE :cocktail',
-                      {'cocktail': '%' + cocktail + '%'})
-            matching_order = c.fetchone()
-        # If order is found, delete the order from the database and return info about the matching order
+        db_cursor.execute('SELECT * FROM orders WHERE message LIKE :cocktail',
+                          {'cocktail': '%' + cocktail + '%'})
+        matching_order = db_cursor.fetchone()
         if matching_order:
-            # Delete the order from the database
-            with conn:
-                c.execute('''DELETE FROM orders
-                  WHERE message = :message
-                  AND user_id = :user_id
-                  AND timestamp = :timestamp
-                  LIMIT 1''', {'message': matching_order[0], 'user_id': matching_order[1], 'timestamp': matching_order[2]})
-            # Return info about the matching order
-            return json.dumps({'user_id': matching_order[1], 'cocktail': cocktail, 'timestamp': matching_order[2]})
-    # If no order is found, save the rule in the database and wait for it to be fulfilled
-    with conn:
-        c.execute('INSERT INTO rules VALUES (:strings_to_match, :callback)', {
-                  'strings_to_match': strings_to_match, 'callback': callback_url})
-    # Send a response to the CPEE indicating that it should wait for the rule to be fulfilled
-    response.set_header('Cpee-Callback', 'true')
-    return 'No matching order found, rule saved in rule queue'
+            return matching_order
+    return None
+
+
+# Delete order from database
+
+def delete_order(db_cursor, order):
+    db_cursor.execute('''DELETE FROM orders
+                    WHERE message = :message
+                    AND user_id = :user_id
+                    AND timestamp = :timestamp
+                    LIMIT 1''', {'message': order[0], 'user_id': order[1], 'timestamp': order[2]})
+
+
+# Save rule in database
+
+def save_rule(db_cursor, strings_to_match, callback_url):
+    db_cursor.execute('INSERT INTO rules VALUES (:strings_to_match, :callback)', {
+        'strings_to_match': strings_to_match, 'callback': callback_url})
 
 
 run(host='::1', port=49125)
